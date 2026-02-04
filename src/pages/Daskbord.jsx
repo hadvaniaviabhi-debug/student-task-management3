1e8a793db0c8faf467049693594d1084ad275b81
hadvaniaviabhi-book <hadvaniaviabhi@gmail.com>
2026-02-04 15:48:31 +0530

    [HA][This is the add task]

--- src/pages/Daskbord.jsx
+++ src/pages/Daskbord.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+// …existing code…//
+import React, { useEffect, useState } from 'react'
+import Navbar from '../componentes/Navbar'
+import { useNavigate } from 'react-router-dom'
+import TaskList from '../componentes/TaskList'
 
 const Daskbord = () => {
+
+const navigate = useNavigate()
+const [tasks, setTasks] = useState([])
+
+useEffect(()=>{
+fetchData();
+},[]);
+
+const fetchData = async() =>{
+try {
+const response = await fetch("http://localhost:3000/tasks");
+const data = await response.json();
+setTasks(data);
+}catch(error) {
+console.log(error)
+}
+};
+
+const handleLogout = () => {
+console.log('click from dashboard')
+localStorage.removeItem('loginData')
+localStorage.removeItem('authData')
+navigate('/login')
+}
+
 return (
 <div>
-<h1>hello React…!</h1>
+<Navbar title="Task Management" onLogout={handleLogout}/>
+<h1> Hello </h1>
+<TaskList tasks={tasks} />
 </div>
 )
 }
 
 export default Daskbord
-
-
+// …existing code…
